a791f056aecdd6148ab7376fa1ee7f4a951052c0
ChristianMinnie <christian.minnie@gmail.com>
2021-10-15 12:41:49 -0500

    Adds User and Tutorial models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "cminnie", email: "cminnie@gmail.com", password_digest: "xyz123")
+User.create(username: "tester", email: "atester@test.com", password_digest: "abc234")
