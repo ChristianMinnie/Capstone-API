ef7301b4d31bdd9ad5c0fb563c29811f0a566cac
ChristianMinnie <christian.minnie@gmail.com>
2021-10-22 10:25:55 -0500

    Adds 5 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get "/tutorials" => "tutorials#index"
-  # # post "/login" => "sessions#create"
+  post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
+
+  get "/tutorials" => "tutorials#index"
+  post "/tutorials" => "tutorials#create"
+
+  post "/ratings" => "ratings#create"
 end
